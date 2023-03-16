c59ef3c2128268efad2008bf8491462d6ad1d2ca
Anthony Whyte <anthwhyte@gmail.com>
2023-03-16 19:33:24 -0400

    Add initial set of files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# dotfiles-m1
-Dotfiles for M1-based machines
+## dotfiles
+
+### special files
+* `.gitignore`: specifies which files to ignore when committing to this repo.
+* `brew/Brewfile`: list of taps, packages and cask apps for Homebrew to install.
+* `scripts/symlinks.sh`: run this file from the command line in order to generate required symbolic links
---
+++ scripts/symlinks.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Command options
+# -s create symbolic link
+# -f continue with other symlinking if error occurs
+# -n avoid symlinking a symlink
+
+# git
+rm -rf /Users/arwhyte/.gitconfig
+ln -nfs /Users/arwhyte/Development/github/arwhyte/dotfiles-m1/git/.gitconfig /Users/arwhyte/.gitconfig
+# rm -rf /Users/arwhyte/.gitconfig.local
+# ln -nfs /Users/arwhyte/Development/github/arwhyte/dotfiles-m1/git/.gitconfig.local /Users/arwhyte/.gitconfig.local
+# rm -rf /Users/arwhyte/.gitignore.global
+# ln -nfs /Users/arwhyte/Development/github/arwhyte/dotfiles-m1/git/.gitignore.global /Users/arwhyte/.gitignore.global
+
+# psql
+rm -rf /Users/arwhyte/.psqlrc
+ln -nfs /Users/arwhyte/Development/github/arwhyte/dotfiles-m1/psql/.psqlrc /Users/arwhyte/.psqlrc
+
+# zsh
+rm -rf /Users/arwhyte/.zprofile
+ln -nfs /Users/arwhyte/Development/github/arwhyte/dotfiles-m1/zsh/.zprofile /Users/arwhyte/.zprofile
+# rm -rf /Users/arwhyte/.zshenv
+# ln -nfs /Users/arwhyte/Development/github/arwhyte/dotfiles-m1/zsh/.zshenv /Users/arwhyte/.zshenv
+rm -rf /Users/arwhyte/.zshrc
+ln -nfs /Users/arwhyte/Development/github/arwhyte/dotfiles-m1/zsh/.zshrc /Users/arwhyte/.zshrc
