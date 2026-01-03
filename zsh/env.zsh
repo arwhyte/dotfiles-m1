482b0b985ab85747791f048727d7c82b3ece9621
Anthony Whyte <anthwhyte@gmail.com>
2026-01-03 14:48:53 -0500

    Fix uv PATH

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,7 +4,7 @@
 export PATH="/opt/homebrew/opt/postgresql@17/bin:$PATH"
 
 # uv
-export PATH="$HOME/.local/share/uv/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 
 # Brewfile
 export HOMEBREW_BREWFILE="$HOME/Development/github/arwhyte/dotfiles-m1/brew/Brewfile"
